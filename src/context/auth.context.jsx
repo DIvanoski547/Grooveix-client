4a76d7ff9f3eef899eac0f8171344544fffc24e4
Omayma El Hadari <om.elhadari@gmail.com>
2024-03-21 10:55:13 +0100

    first approach of auth context

--- src/context/auth.context.jsx
+++ src/context/auth.context.jsx
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from "react";
 
-const AuthContext = React.createContext();
+const AuthContext = React.createContext(); // call createContext() to create our Context object
 
-function AuthProviderWrapper() {
+function AuthProviderWrapper(props) {
 
+    //set necessary states for the authentication process
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // state of being logged in
+    const [isLoading, setIsLoading] = useState(true); // state of loading process while authentication is being done
+    const [user, setUser] = useState(null); // state for storing the user
+
+    //retrieve token from local storage to have it easily accessible
+    const storeToken = (token) => {
+        localStorage.setItem("authToken", token) //set item named authToken from local storage as token
+    };
+
+    
+
+    return (
+        //set the provider, to provide context values to other components, as a wrapper component
+        <AuthContext.Provider value={""}
+
+    );
 }
 
 export { AuthProviderWrapper, AuthContext };
