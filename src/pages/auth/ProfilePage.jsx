3b2579cbc87501e3d0ea20960582015c5d4cb14a
Omayma El Hadari <om.elhadari@gmail.com>
2024-03-26 15:16:11 +0100

    upload image feature visible, still need to figure out the file update

--- src/pages/auth/ProfilePage.jsx
+++ src/pages/auth/ProfilePage.jsx
@@ -3,17 +3,18 @@ import { useState, useContext } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
-import userService from "../../services/user.service";
-import uploadService from "../../services/upload.service";
+import avatarPng from "../../assets/avatar.png";
 
 function ProfilePage() {
   const { user, setUser, isAdmin } = useContext(AuthContext);
   const [imageUpload, setImageUpload] = useState({});
+  const [showUpload, setShowUpload] = useState(false);
 
   const handleFileUpload = (e) => {
     const uploadData = new FormData();
     uploadData.append("image", e.target.files[0]);
     setImageUpload(uploadData);
+    // setShowUpload(false);
   };
 
   const handleSubmit = async (e) => {
@@ -33,30 +34,57 @@ function ProfilePage() {
   return (
     <div>
       <Navbar />
-      <div >
-            <p>{user.username}</p>
-            <p>{user.firstName}</p>
-            <p>{user.lastName}</p>
-        <img src={user.profileImage} />
-        
-      </div>
-      
-      {isAdmin ? (
-        <>
-          <h1>im admin</h1>
-          <button>
-            <Link to="/all-users"> View all users</Link>
-          </button>{" "}
-          <button>
-            <Link to="/albums"> View all albums</Link>
+      <div>
+        <p>{user.username}</p>
+        <p>{user.firstName}</p>
+        <p>{user.lastName}</p>
+
+        {!isAdmin && user.profileImage ? (
+          <img
+            src={user.profileImage}
+            alt={"profile_image"}
+            style={{ width: "50px", height: "50px", borderRadius: "75%" }}
+          />
+        ) : (
+          <img
+            src={avatarPng}
+            alt={"profile_image"}
+            style={{ width: "50px", height: "50px", borderRadius: "75%" }}
+          />
+        )}
+        {!showUpload && (
+          <button onClick={() => setShowUpload(!showUpload)}>
+            Change Profile Image
           </button>
-        </>
-      ) : (
-        <>
+        )}
+      </div>
+      <div>
+        {showUpload && (
+          <form onSubmit={handleSubmit}>
+            <input type="file" onChange={(e) => handleFileUpload(e)} />
+            <button onClick={() => setShowUpload(!showUpload)}>
+              Cancel change
+            </button>
+            <button onClick={()=> setShowUpload(!showUpload)} type="submit">Save new profile image</button>
+          </form>
+        )}
+      </div>
+      <div>
+        {isAdmin ? (
+          <>
+            <button>
+              <Link to="/all-users"> View all users</Link>
+            </button>{" "}
+            <button>
+              <Link to="/albums"> View all albums</Link>
+            </button>
+          </>
+        ) : (
+          <>
             <h1>not admin</h1>
-            
-        </>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
