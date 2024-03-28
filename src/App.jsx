c9e0fc01426d026224978077da1ffb947833d759
David Ivanoski <davidivanoski1@live.com>
2024-03-28 15:04:56 +0100

    homepage changed, artist details page code added, small changes in album details page

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import LoginPage from "./pages/auth/LoginPage";
 import SignupPage from "./pages/auth/SignupPage";
 import Homepage from "./pages/Homepage";
 import StarterPage from "./pages/StarterPage";
-import AlbumDetailPage from "./pages/AlbumDetailsPage";
 import AlbumsListPage from "./pages/AlbumsListPage";
+import AlbumDetailsPage from "./pages/AlbumDetailsPage";
 import AlbumCreatePage from "./pages/AlbumCreatePage";
+import ArtistDetailsPage from "./pages/ArtistDetailsPage";
 import ProfilePage from "./pages/auth/ProfilePage";
 import AllUsersPage from "./pages/AllUsersPage";
 import AlbumEditPage from "./pages/AlbumEditPage";
@@ -21,10 +22,11 @@ function App() {
         <Route exact path="/signup" element={<SignupPage />} />
         <Route exact path="/homepage" element={<Homepage />} />
         <Route exact path="/albums" element={<AlbumsListPage />} />
-        <Route exact path="/albums/:albumId" element={<AlbumDetailPage />} />
+        <Route exact path="/albums/:albumId" element={<AlbumDetailsPage />} />
         <Route exact path="/albums/create-album" element={<AlbumCreatePage />} />
         <Route exact path="/albums/edit/:albumId" element={<AlbumEditPage />} />
         <Route exact path="/profile" element={<ProfilePage />} />
+        <Route exact path="/artists/:artistId" element={<ArtistDetailsPage />} />
         <Route exact path="/all-users" element={<AllUsersPage />} />
       </Routes>
     </div>
