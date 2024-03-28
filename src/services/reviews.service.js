7d26f6e1a865c9fdd930a46b415235129210c062
David Ivanoski <davidivanoski1@live.com>
2024-03-28 20:04:52 +0100

    small changes

--- src/services/reviews.service.js
+++ src/services/reviews.service.js
@@ -4,7 +4,7 @@ class ReviewsService {
   constructor() {
     // Create a new instance of axios with a custom configuration
     this.api = axios.create({
-      baseURL: import.meta.env.SERVER_URL || "http://localhost:5005",
+      baseURL: import.meta.env.VITE_SERVER_URL || "http://localhost:5005",
     });
     // Automatically set JWT token in the headers for every request
     this.api.interceptors.request.use((config) => {
