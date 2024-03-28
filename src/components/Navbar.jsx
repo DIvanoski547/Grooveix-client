31ed4eb5de4f33af77a55ab959836992eb499f5e
Omayma El Hadari <om.elhadari@gmail.com>
2024-03-28 12:08:48 +0100

    first steps styling navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,47 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
 
 function Navbar() {
-  const { isLoggedIn, logOutUser } = useContext(AuthContext);
+  const { user, isLoggedIn, logOutUser } = useContext(AuthContext);
   return (
-    <nav>
-      <Link to="/homepage">
-        <button>Home</button>
-      </Link>
+    <nav className="p-3 mb-3 border-bottom">
+      <div className="container">
+        <div d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start>
+          <a href="/homepage" className="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+            <svg className="bi me-2"></svg>
+          </a>
+          <div>
+            <NavLink to="/homepage">
+              <button>Home</button>
+            </NavLink>
 
-      {isLoggedIn ? (
-        <>
-          <Link to="/profile">
-            {" "}
-            <button>Profile</button>{" "}
-          </Link>
-          <button onClick={logOutUser}>
-            <Link to="/">Logout</Link>
-          </button>
-        </>
-      ) : (
-        <>
-          <Link to="/signup">
-            {" "}
-            <button>Sign Up</button>{" "}
-          </Link>
-          <Link to="/login">
-            {" "}
-            <button>Login</button>{" "}
-          </Link>
-        </>
-      )}
+            {isLoggedIn ? (
+              <>
+                <NavLink to="/profile">
+                  {" "}
+                  <button>Profile</button>{" "}
+                </NavLink>
+
+                <NavLink to="/">
+                  <button onClick={logOutUser}>Logout</button>
+                </NavLink>
+              </>
+            ) : (
+              <>
+                <NavLink to="/signup">
+                  {" "}
+                  <button>Sign Up</button>{" "}
+                </NavLink>
+                <NavLink to="/login">
+                  {" "}
+                  <button>Login</button>{" "}
+                </NavLink>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
